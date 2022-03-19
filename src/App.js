8c50dcc2999cfbd32ef65f5a5c73842116f74c4d
songulija <65647664+songulija@users.noreply.github.com>
2022-03-19 19:06:35 +0200

    addded logo to this website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
-import {Helmet } from 'react-helmet'
 function App() {
   return (
     <Router>
-      <Helmet>
-        <title>Rivita - Krovinių gabenimas geležinkelio transportu</title>
-        <meta name='description' content="Krovinių ekspedijavimas geležinkelio transportu"/>
-      </Helmet>
       <HomeScreen />
     </Router>
   );
